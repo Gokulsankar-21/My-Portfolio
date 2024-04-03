789e30787075c38fb26df351f316e12740129f8b
GokulDev <gokulsankar@gamil.com>
2024-04-03 14:40:17 +0530

    add the big blurrydots to the background

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+      <header>
+          <div className="fixed top-0 left-1/2 h-[4.5rem] w-full 
+          rounded-none border "></div>
+    </header>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,14 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={` ${inter.className} 
+      bg-gray-50 text-gray-950  relative
+      `}>
+        
+          {/* big blurry dots -  trendy design */}
+
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full  sm:w-[68.75rem] blur-[10rem] -z-10"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full  sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] blur-[10rem] -z-10"></div>
+        {/* header */}
+        <Header />
         {/* Home Page */}
         {children}
       </body>
     </html>
   )
 }
-
 /**
  * slogo for me - 
  * i am always working with project, 
@@ -34,5 +43,14 @@ export default function RootLayout({
  * 
  * ithu byteGrad - excellent and cool ah soldraru
  * na itha epdi expose panna poren nu - enaku idea ila 
- * ivaru kita - trendy and catchy learning ah kahtukalam -  modern dev ah na iruka mudyum
+ * ivaru kita - trendy and catchy learning ah kahtukalam -  modern dev ah na iruka 
+ * mudyum 
+ * 
+ * @backtick
+ * backtick - windows + `` this
+ */
+/**
+ * @style_notes
+ * mobile and tab size ku etha marri nama dots ah pakkathula right each other ;la set pandrom
+ * in large screeen la gap perusa iruku so atheketha maari ella screen kum alignment ah set pandrom - Good Tricks
  */
