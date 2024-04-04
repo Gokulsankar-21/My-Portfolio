a1e1554dae6f933e7fdb08595d87124ed8fd7672
Gokulsankar Thangaraj <gokulsankar2002t@gmail.com>
2024-04-04 04:29:47 +0000

    complete the into component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,9 +2,13 @@
 import Image from 'next/image'
 import React from 'react'
 import {motion} from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithub, FaGithubSquare } from 'react-icons/fa'
 export default function Intro() {
   return (
-    <section>
+    <section className='max-w-[50rem] text-center '>
         <div className="relative flex items-center justify-center">
             <motion.div 
             initial={{opacity:0,scale:0}}
@@ -20,20 +24,69 @@ export default function Intro() {
                  border-[0.35rem] border-white'/>
             </motion.div>
             <motion.div 
-            initial={{opacity:0,scale:0}}
-            animate={{opacity:1,scale:1}}
+            initial={{opacity:0,x:'40px',scale:0}}
+            animate={{opacity:1,x:'40px',scale:1}}
             transition={{
                 type:'spring',
                 delay:0.1,
                 stiffness:125,
                 duration:0.7
             }}
-            className=' absolute bottom-1 right-2'
+            className=' absolute bottom-0 right-1/2'
             >
                 <Image src='/hello-img.png' alt='hello' width='100' height='100'  quality={'95'} priority={true}
                  className='w-9 h-9  '/>
             </motion.div>
         </div>
+        {/* Text */}
+        <motion.p 
+        className='mt-4 mb-10 px-4 text-2xl sm:text-3xl  font-medium !leading-[1.5] '
+        
+        initial={{opacity:0,y:100}}
+        animate={{opacity:1,y:0}}>
+            <span className='font-bold'>
+                Hello, I'm Gokulsankar.
+            </span> I'm a  {' '}
+            <span className='font-bold'>
+                full-stack-developer
+            </span> with {' '}
+            <span className='font-bold'>
+                hands-on 
+            </span> {' '}experience. I enjoy building <span className='italic'>
+                web apps and sites
+            </span>. My focus is {' '}
+            <span className='underline cursor-pointer'>React (Next.js)</span>
+        </motion.p>
+        {/* Actions */}
+        <motion.div className='flex flex-col sm:flex-row
+         items-center justify-center gap-2 font-medium text-lg'
+         initial={{opacity:0,y:100}}
+         animate={{opacity:1,y:0}}
+         >
+             <Link href='#contact' 
+             className='group bg-gray-900 text-white px-7 py-3 rounded-full flex items-center justify-center gap-2 outline-none focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950 transition'>
+                Contact me here <BsArrowRight  className='opacity-80 group-hover:translate-x-1' />
+             </Link>
+             <a className='group text-gray-700 px-7 bg-white py-3 rounded-full flex items-center justify-center gap-2 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10'
+             href='/cv.pdf' 
+             download
+             >
+               Download CV <HiDownload  className='opacity-70 group-hover:translate-y-1'/> 
+             </a>
+
+             <a className='text-gray-700 p-4 bg-white rounded-full flex items-center justify-center
+             outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 hover:text-gray-950'
+             
+             href='https://github.com/Gokulsankar-21' target='_blank'>
+                <BsLinkedin />
+             </a>
+
+             <a className='text-gray-700 p-4 bg-white rounded-full flex items-center justify-center text-[1.35rem]
+             outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 hover:text-gray-950'
+             href='https://github.com/Gokulsankar-21' target='_blank'>
+                <FaGithubSquare  />
+             </a>
+        </motion.div>
     </section>
   )
 }
@@ -47,4 +100,20 @@ export default function Intro() {
  * 
  ennoda manathiraila iT industry pathi na oru visuaalize  panni iruken
  * 
+ */
+/**
+ * @portfolio_ideas
+ * en portfolio la cloud ku nu thani oru section 
+ */
+
+/**
+ * 
+ * root route la public um add aguum
+ * https://3000-gokulsankar-myportfolio-78hbq4427o5.ws-us110.gitpod.io/cv.pdf
+ * 
+ * ithu nama route ah manage pannra maari 
+ * static files elam root route la access aguthu 
+ */
+/**
+ * nice hover effect m, nice focus effect, nice active effect
  */
